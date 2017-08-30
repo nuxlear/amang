f8197c21a817c16fdfded69ef63b2459c1d3357d
nuxlear <nuclear1221@gmail.com>
2017-08-31 04:54:19 +0900

    delete obsolute venv

--- amangenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/nuxlear/Documents/GitHub/nuxlear/Python/django/projects/Amang/amangenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
