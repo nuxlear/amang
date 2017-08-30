457ee513150794c4ad9cd7a4321515d1f4810d07
nuxlear <nuclear1221@gmail.com>
2017-08-31 05:06:02 +0900

    add allowed host op

--- Amang/settings.py
+++ Amang/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o&9a!@=pva24l*l(#fuh1tk4y5^01dsaesu+9%%-q1stl%q3ut'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
